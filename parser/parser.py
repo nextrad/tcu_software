d26f2f41d4e51ddbae1093203a33a6b93b670fb3
bradkahn <bradkahn92@gmail.com>
2018-06-12 11:59:20 +0200

    make file_name arg optional, fix set_tcu_params(), initialise parser

--- parser/parser.py
+++ parser/parser.py
@@ -6,18 +6,19 @@ import sys
 
 class HeaderFileParser(object):
     """NeXtRAD.ini File Parser"""
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self, file_name = ''):
         self.logger = logging.getLogger('header_file_parser_logger')
         self.file_parser = configparser.ConfigParser()
 
-        self.file_parser['PulseParameters'] = {'WAVEFORM_INDEX': '',
-                                               'NUM_PRIS': '',
-                                               'PRE_PULSE': '',
-                                               'PRI_PULSE_WIDTH': '',
-                                               'X_AMP_DELAY': '',
-                                               'L_AMP_DELAY': '',
-                                               'PULSES': ''}
+        self.file_parser['PulseParameters'] = {'WAVEFORM_INDEX': '0',
+                                               'NUM_PRIS': '0',
+                                               'PRE_PULSE': '0',
+                                               'PRI_PULSE_WIDTH': '0',
+                                               'X_AMP_DELAY': '0',
+                                               'L_AMP_DELAY': '0',
+                                               'PULSES': '0,0,0,0'}
+        if file_name != '':
+            self.read_header(file_name)
 
     def __str__(self):
         params = ''
@@ -30,17 +31,17 @@ class HeaderFileParser(object):
         params += 'pulses: ' + self.file_parser['PulseParameters']['PULSES']
         return params
 
-    def read_header(self):
+    def read_header(self, file_name):
         """ Parses a given header file for TCU parameters """
 
-        self.header_file = self.file_parser.read(self.file_name)
+        self.header_file = self.file_parser.read(file_name)
         if len(self.header_file) > 0:
             if not self.file_parser.has_section('PulseParameters'):
                 self.logger.error('No "PulseParameters" section found in '
-                                  'header file "{}"'.format(self.file_name))
+                                  'header file "{}"'.format(file_name))
         else:
             self.logger.error('Could not find header file "{}", no '
-                              'parameters extracted'.format(self.file_name))
+                              'parameters extracted'.format(file_name))
 
     def get_tcu_params(self):
         """ Returns a dictionary containing TCU parameters
@@ -60,33 +61,25 @@ class HeaderFileParser(object):
                                             'frequency'     ->  float
         """
         tcu_params = dict()
-        if len(self.header_file) > 0:
-            if self.file_parser.has_section('PulseParameters'):
-                pulses = self._extract_param('PULSES')
-                pulses = pulses.replace('"', '')
-                pulses_list = pulses.split('|')
-                tcu_params['num_pulses'] = len(pulses_list)
-                num_pris = eval(self._extract_param('NUM_PRIS'))
-                tcu_params['num_repeats'] = num_pris//tcu_params['num_pulses']
-                tcu_params['pri_pulse_width'] = eval(self._extract_param('PRI_PULSE_WIDTH'))
-                tcu_params['pre_pulse'] = eval(self._extract_param('PRE_PULSE'))
-                tcu_params['x_amp_delay'] = eval(self._extract_param('X_AMP_DELAY'))
-                tcu_params['l_amp_delay'] = eval(self._extract_param('L_AMP_DELAY'))
-                tcu_params['pulses'] = []
-                for pulse in pulses_list:
-                    pulse_params = pulse.split(',')
-                    pulse_param_dict = {'pulse_width':eval(pulse_params[0]),
-                                        'pri':eval(pulse_params[1]),
-                                        'pol_mode':eval(pulse_params[2]),
-                                        'frequency':eval(pulse_params[3])}
-                    tcu_params['pulses'].append(pulse_param_dict)
-            else:
-                self.logger.error('No "PulseParameters" section found in '
-                                  'header file "{}"'.format(self.file_name))
-        else:
-            self.logger.error('Could not find header file "{}", no '
-                              'parameters extracted'.format(self.file_name))
-
+        if self.file_parser.has_section('PulseParameters'):
+            pulses = self._extract_param('PULSES')
+            pulses = pulses.replace('"', '')
+            pulses_list = pulses.split('|')
+            tcu_params['num_pulses'] = len(pulses_list)
+            num_pris = eval(self._extract_param('NUM_PRIS'))
+            tcu_params['num_repeats'] = num_pris//tcu_params['num_pulses']
+            tcu_params['pri_pulse_width'] = eval(self._extract_param('PRI_PULSE_WIDTH'))
+            tcu_params['pre_pulse'] = eval(self._extract_param('PRE_PULSE'))
+            tcu_params['x_amp_delay'] = eval(self._extract_param('X_AMP_DELAY'))
+            tcu_params['l_amp_delay'] = eval(self._extract_param('L_AMP_DELAY'))
+            tcu_params['pulses'] = []
+            for pulse in pulses_list:
+                pulse_params = pulse.split(',')
+                pulse_param_dict = {'pulse_width':eval(pulse_params[0]),
+                                    'pri':eval(pulse_params[1]),
+                                    'pol_mode':eval(pulse_params[2]),
+                                    'frequency':eval(pulse_params[3])}
+                tcu_params['pulses'].append(pulse_param_dict)
         return tcu_params
 
     def _extract_param(self, param):
@@ -121,17 +114,14 @@ class HeaderFileParser(object):
                                             'frequency'     ->  float
         """
         # TODO: check that all the required items exist in the params argument
-        self.file_parser['PulseParameters']['PULSES'] = '""'
-        test = '"'
+        self.file_parser['PulseParameters']['PULSES'] = '"'
         for index, pulse in enumerate(params['pulses']):
             self.file_parser['PulseParameters']['PULSES'] += str(pulse['pulse_width'])+','+str(pulse['pri'])+','+str(pulse['pol_mode'])+','+str(pulse['frequency'])
-            test += str(pulse['pulse_width'])+','+str(pulse['pri'])+','+str(pulse['pol_mode'])+','+str(pulse['frequency'])
             if index < (len(params['pulses']) - 1):
                 self.file_parser['PulseParameters']['PULSES'] += '|'
-                test += '|'
-        test += '"'
-        print(test)
-        self.file_parser['PulseParameters']['NUM_PRIS'] = ''
+        self.file_parser['PulseParameters']['PULSES'] += '"'
+
+        self.file_parser['PulseParameters']['NUM_PRIS'] = str(params['num_pulses'] * params['num_repeats'])
         self.file_parser['PulseParameters']['PRI_PULSE_WIDTH'] = str(params['pri_pulse_width'])
         self.file_parser['PulseParameters']['PRE_PULSE'] = str(params['pre_pulse'])
         self.file_parser['PulseParameters']['X_AMP_DELAY'] = str(params['x_amp_delay'])
@@ -139,14 +129,13 @@ class HeaderFileParser(object):
 
 
 if __name__ == '__main__':
-    # hfparser = HeaderFileParser('../../nextrad_header/NeXtRAD.ini')
-    hfparser = HeaderFileParser('../../nextrad_header/NeXtRAD')
-
+    hfparser = HeaderFileParser()
+    hfdir = '../../nextrad_header/NeXtRAD.ini'
     test_params = {'pre_pulse': 30,
                    'x_amp_delay': 3.5,
                    'l_amp_delay': 1.0,
                    'num_pulses': 2,
                    'num_repeats': 75000,
-                   'pri_pulse_width': 500,
+                   'pri_pulse_width': 100,
                    'pulses': [{'pol_mode': 4, 'frequency': 8500.0, 'pri': 500.0, 'pulse_width': 10.0},
                               {'pol_mode': 5, 'frequency': 8500.0, 'pri': 500.0, 'pulse_width': 10.0}]}
