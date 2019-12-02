a3928131d9fe8e3585d20e6b805baeb368637a97
Brad Kahn <khnbra003@myuct.ac.za>
2019-12-02 10:48:22 +0200

    copy pasta bug, tcu actually disarms

--- controller_v2.py
+++ controller_v2.py
@@ -330,8 +330,6 @@ class TCUController(harpoon.Project):
             if fpga_con.running():
                 self.logger.info('disarming tcu...')
                 reg_instruction.write(0)
-                # time.sleep(3)
-                reg_instruction.write(1)
                 if self.voice:
                     os.system('spd-say -t female1 -i -0 "disarmed" -r -30 -p -30')
             else:
